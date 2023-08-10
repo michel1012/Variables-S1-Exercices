123799590f88d9add9793f9cb7bc5a8c59307cb2
michel1012 <96859667+michel1012@users.noreply.github.com>
2023-08-10 16:44:43 +0200

    08-10-2023 15:41:51 PM

--- Module2/src/Main2.java
+++ Module2/src/Main2.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main2 {
     public static void main(String... args){
         System.out.println("dsdsdsd");
+        Scanner keyb = new Scanner(System.in);
+        int n = keyb.nextInt();
+        System.out.println(n);
     }
 }
 
